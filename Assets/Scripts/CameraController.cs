8f28ffbaa6d603df17e276a95668fd8fc603cd4e
Anselm Brehme <development.anselm@breh.me>
2017-04-21 15:39:27 +0200

    Messing with stuff and things

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,11 +6,9 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 }
---
+++ Assets/Scripts/ClickMyTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMyTile : MonoBehaviour
+{
+    public GameObject tehPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000, 1 << 9)) // Level 9 = Floor
+            {
+                var agent = tehPlayer.GetComponent<NavMeshAgent>();
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
