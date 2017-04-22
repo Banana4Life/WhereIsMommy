9810190d7b200ce602f44d1b9e21641c2f91727b
Anselm Brehme <development.anselm@breh.me>
2017-04-22 20:07:07 +0200

    carry this Teddy!

---
+++ Assets/TeddyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeddyController : MonoBehaviour
+{
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var playerController = other.gameObject.GetComponentInParent<PlayerController>();
+
+        if (playerController != null)
+        {
+            gameObject.SetActive(false);
+            playerController.carryTeddy = true;
+            // TODO show StoryText
+        }
+    }
+}
