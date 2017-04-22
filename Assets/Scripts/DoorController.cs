5f4132204ea917ff4836b2e978370c368e31be13
Phillip Schichtel <phillip.public@schich.tel>
2017-04-22 19:14:11 +0200

    smooth doors

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -8,7 +8,10 @@ public class DoorController : MonoBehaviour
     public String RequiredKey;
     public Vector3 RelativeRotationAxis = Vector3.zero;
     public bool ClockWise = true;
+    public float RotationSpeed = 45f;
     private bool isOpen = false;
+    private bool changing = false;
+    private Quaternion targetRotation;
 
     // Use this for initialization
     void Start()
@@ -18,6 +21,14 @@ public class DoorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (changing)
+        {
+            gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+            if (targetRotation == gameObject.transform.rotation)
+            {
+                changing = false;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision other)
@@ -33,8 +44,9 @@ public class DoorController : MonoBehaviour
         if (open != isOpen)
         {
             isOpen = open;
-            gameObject.transform.RotateAround(gameObject.transform.position + RelativeRotationAxis, Vector3.up,
-                90 * (ClockWise && open ? 1 : -1));
+            changing = true;
+            var direction = 90 * (ClockWise && open ? 1 : -1);
+            targetRotation = gameObject.transform.rotation * Quaternion.Euler(Vector3.up * direction);
         }
     }
 }
