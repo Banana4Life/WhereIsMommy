08c2346552294e9fd727d19b26e0cb2325cadcb7
Phillip Schichtel <phillip.public@schich.tel>
2017-04-22 14:32:54 +0200

    Added model cycleer

---
+++ Assets/Scripts/ModelCycler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelCycler : MonoBehaviour
+{
+    public float modelDuration;
+    public List<Mesh> meshes;
+    private MeshFilter meshFilter;
+    private int index;
+    public bool startOnAttach = true;
+
+	// Use this for initialization
+	void Start () {
+	    meshFilter = gameObject.GetComponent<MeshFilter>();
+	    if (startOnAttach)
+	    {
+	        StartCycling();
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Animate()
+    {
+        meshFilter.mesh = meshes[index];
+        index = (index + 1) % meshes.Count;
+    }
+
+    public bool IsCycling()
+    {
+        return IsInvoking("Animate");
+    }
+
+    public void StartCycling(int startIndex = 0)
+    {
+        if (!IsCycling())
+        {
+            index = startIndex;
+            InvokeRepeating("Animate", 0, modelDuration);
+        }
+    }
+
+    public void StopCyclign()
+    {
+        CancelInvoke("Animate");
+    }
+}
