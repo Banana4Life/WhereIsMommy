7e66ec90aebf91ec28d43b2f757466f5485facaa
Phillip Schichtel <phillip.public@schich.tel>
2017-04-22 16:18:50 +0200

    Movement?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
     public Camera cam;
 
     [Header("Settings")]
-    public float speed = 0.35f;
+    public float Thrust = 2000;
+    public float Epsilon = 0.1f;
 
     [Header("State")]
     public bool carryTeddy = false;
@@ -26,14 +27,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateToMouse();
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
 
-        RaycastHit hit;
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 1000, 1 << 9)) // Level 9 = Floor
+        var rb = GetComponent<Rigidbody>();
+        if (Math.Abs(x) > Epsilon || Math.Abs(y) > Epsilon)
         {
-            var hitPoint = hit.point;
-            hitPoint.y = gameObject.transform.position.y;
-            gameObject.transform.LookAt(hitPoint);
+            rb.AddForce(new Vector3(x, 0, y).normalized * Thrust * Time.deltaTime);
+        }
+        else
+        {
+            var change = new Vector3(rb.velocity.x / -2, 0, rb.velocity.z / -2);
+            rb.AddForce(change, ForceMode.VelocityChange);
         }
 
 
@@ -55,39 +61,51 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void RotateToMouse()
+    {
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000, 1 << 9)) // Level 9 = Floor
+        {
+            var hitPoint = hit.point;
+            hitPoint.y = gameObject.transform.position.y;
+            gameObject.transform.LookAt(hitPoint);
+        }
+    }
+
     void FixedUpdate()
     {
-        if (panic < 100)
-        {
-            var x = Input.GetAxis("Horizontal");
-            var y = Input.GetAxis("Vertical");
-
-            if (Math.Abs(x) < 0.2)
-            {
-                x = 0;
-            }
-            if (Math.Abs(y) < 0.2)
-            {
-                y = 0;
-            }
-//            Debug.Log(x + ":" + y);
-
-//            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(x * speed, 0, y * speed);
-            var targetPos = gameObject.transform.position + new Vector3(x, 0, y).normalized * speed;
-            //gameObject.GetComponent<Rigidbody>().MovePosition(targetPos);
-
-            var agent = gameObject.GetComponent<NavMeshAgent>();
-            if (x == 0 && y == 0)
-            {
-                agent.isStopped = true;
-            }
-            else
-            {
-                agent.isStopped = false;
-                agent.SetDestination(targetPos);
-            }
-
-        }
+//        if (panic < 100)
+//        {
+//            var x = Input.GetAxis("Horizontal");
+//            var y = Input.GetAxis("Vertical");
+//
+//            if (Math.Abs(x) < 0.2)
+//            {
+//                x = 0;
+//            }
+//            if (Math.Abs(y) < 0.2)
+//            {
+//                y = 0;
+//            }
+////            Debug.Log(x + ":" + y);
+//
+////            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(x * speed, 0, y * speed);
+//            var targetPos = gameObject.transform.position + new Vector3(x, 0, y).normalized * speed;
+//            //gameObject.GetComponent<Rigidbody>().MovePosition(targetPos);
+//
+//            var agent = gameObject.GetComponent<NavMeshAgent>();
+//            if (x == 0 && y == 0)
+//            {
+//                agent.isStopped = true;
+//            }
+//            else
+//            {
+//                agent.isStopped = false;
+//                agent.SetDestination(targetPos);
+//            }
+//
+//        }
     }
 
 }
