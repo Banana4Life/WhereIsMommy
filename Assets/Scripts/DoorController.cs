bd1dffa78456910b448b6b7eab155adf7f25f4e9
Phillip Schichtel <phillip.public@schich.tel>
2017-04-22 18:16:02 +0200

    added an initial door that opens upon collision

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public Vector3 RelativeRotationAxis = Vector3.zero;
+    public bool ClockWise = true;
+    private bool isOpen = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponentInParent<PlayerController>())
+        {
+            ChangeState(true);
+        }
+    }
+
+    public void ChangeState(bool open)
+    {
+        if (open != isOpen)
+        {
+            isOpen = open;
+            gameObject.transform.RotateAround(gameObject.transform.position + RelativeRotationAxis, Vector3.up,
+                90 * (ClockWise && open ? 1 : -1));
+        }
+    }
+}
