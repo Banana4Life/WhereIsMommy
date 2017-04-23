3884aacb9cb06de75ddfa689365189e824dfc471
Anselm Brehme <development.anselm@breh.me>
2017-04-23 14:11:25 +0200

    global light off on game-start

---
+++ Assets/Scripts/TurnOffFullLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffFullLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gameObject.GetComponent<Light>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
