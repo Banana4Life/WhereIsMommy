f04dfc17ffc62104cb87b3a028aae967e0546a36
Anselm Brehme <development.anselm@breh.me>
2017-04-23 18:44:54 +0200

    uv light and trying to make it work on linux

--- Assets/Scripts/CarryScript.cs
+++ Assets/Scripts/CarryScript.cs
@@ -7,12 +7,14 @@ public class CarryScript : MonoBehaviour {
     [Header("CarriedStuff")]
     public GameObject teddy;
     public GameObject flashlight;
+    public GameObject uvLight;
     public GameObject key1;
     public GameObject key2;
 
     [Header("State")]
     public bool carryTeddy;
     public bool carryLight;
+    public bool carryUvLight;
     public bool carryKey1;
     public bool carryKey2;
     public int carryMatches = 0;
@@ -22,6 +24,7 @@ public class CarryScript : MonoBehaviour {
 	{
 	    carryTeddy = false;
 	    carryLight = false;
+	    carryUvLight = false;
 	    carryKey1 = false;
 	    carryKey2 = false;
 	    carryMatches = 0;
@@ -36,6 +39,7 @@ public class CarryScript : MonoBehaviour {
     {
         teddy.SetActive(carryTeddy);
         flashlight.SetActive(carryLight);
+        uvLight.SetActive(carryUvLight);
         key1.SetActive(carryKey1);
         key2.SetActive(carryKey2);
     }
--- Assets/Shader/Lighting Only.shader
+++ Assets/Shader/Lighting Only.shader
@@ -1,6 +1,6 @@
 ﻿Shader "Lighting Only" {
     Properties {
-        _Color ("Main Color", Color) = (1,1,1,1)
+        _Color ("Main Color", Color) = (1, 1, 1, 1)
         _MainTex ("Base (RGB)", 2D) = "white" {}
     }
 
