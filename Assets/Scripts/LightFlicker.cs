8fa4b9159402370a7844a7e6d4476ba40dd815b3
Jonas Dann <jonas.chr.dann@gmail.com>
2017-04-23 12:53:33 +0200

    Added light flicker

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+
+    private Light light;
+    private float timeLeft;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    light = gameObject.GetComponentInChildren<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    timeLeft -= Time.deltaTime;
+	    if (timeLeft < 0) {
+	        light.intensity = 3f + 1.5f * Random.value;
+	        timeLeft = 0.15f;
+	    }
+	}
+}
