1fd3da73a1ab2e068fb1333fe255c4bbe1535be1
Anselm Brehme <development.anselm@breh.me>
2017-04-23 23:45:51 +0200

    terminalcontroller

---
+++ Assets/TerminalController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalController : CollisionController {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void handle(PlayerController pc)
+    {
+        TextController.Get().ShowText("It reads: C B A", Color.red, 10f);
+    }
+}
