dcb9faf8997c853c90b90577aa71d002f4c84bad
Phillip Schichtel <phillip.public@schich.tel>
2017-04-24 00:17:38 +0200

    initial button stuff

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : CollisionController
+{
+    private Light statusLight;
+    private char letter;
+    private Texture texture;
+    private Texture[] textures;
+
+    // Use this for initialization
+    void Start()
+    {
+        statusLight = GetComponentInChildren<Light>();
+    }
+
+    public void SetButtonLetter(char letter)
+    {
+        this.letter = letter;
+        int textureOffset = 'A' - letter;
+        texture = textures[textureOffset];
+    }
+
+    protected override void handle(PlayerController playerController)
+    {
+        //playerController.OnButtonPressed(letter);
+    }
+}
