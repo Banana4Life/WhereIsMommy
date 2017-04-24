867e652228839d94739a0c1578f5fe1322d3bf7a
Jonas Dann <jonas.chr.dann@gmail.com>
2017-04-24 20:20:49 +0200

    Can now play game from start to finish

--- Assets/BigDoorController.cs
+++ Assets/BigDoorController.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class BigDoorController : CollisionController {
+public class BigDoorController : CollisionController
+{
+
+    public GameObject blend;
+
+    private float blendSpeed = 0.1f;
+    private bool triggered = false;
 
 	// Use this for initialization
 	void Start () {
@@ -11,15 +19,23 @@ public class BigDoorController : CollisionController {
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (triggered)
+	    {
+	        Color oldColor = blend.GetComponent<Image>().color;
+	        blend.GetComponent<Image>().color = new Color(oldColor.r, oldColor.g, oldColor.b, oldColor.a + blendSpeed);
+
+	        if (blend.GetComponent<Image>().color.a + blendSpeed > 1)
+	        {
+	            SceneManager.LoadScene("End");
+	        }
+	    }
 	}
 
     protected override void handle(PlayerController pc)
     {
         if (pc.buttonsPressed == 3)
         {
-            Debug.Log("Trigger Ending...");
-            // TODO trigger end scene
+            triggered = true;
         }
     }
 }
