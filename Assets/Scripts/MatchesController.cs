3ce3e9390990d1dcc36dc7ceaca16bb147fde02a
Phillip Schichtel <phillip.public@schich.tel>
2017-04-24 17:17:15 +0200

    matches pickup sound and new background

--- Assets/Scripts/MatchesController.cs
+++ Assets/Scripts/MatchesController.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class MatchesController : CollisionController
 {
+    public AudioClip[] audioClips;
+
     public int amount = 2;
     protected override void handle(PlayerController playerController)
     {
+        PlaySound(playerController);
         gameObject.SetActive(false);
         playerController.Carry().carryMatches += amount;
         TextController.Get().ShowText("Now I can light a candle", TextController.red, 4f);
+    }
+
+    private void PlaySound(PlayerController player)
+    {
+        var clip = audioClips[Random.Range(0, audioClips.Length)];
+        player.gameObject.GetComponent<AudioSource>().PlayOneShot(clip);
     }
 }
