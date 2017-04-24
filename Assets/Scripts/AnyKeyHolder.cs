8ac10063b031dc6314e39fefdbf9d334b725d51c
Phillip Schichtel <phillip.public@schich.tel>
2017-04-25 01:00:38 +0200

    import cleanup

--- Assets/Scripts/AnyKeyHolder.cs
+++ Assets/Scripts/AnyKeyHolder.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 public class AnyKeyHolder : KeyHolder
 {
     public override bool MayOpen(string requiredKey)
