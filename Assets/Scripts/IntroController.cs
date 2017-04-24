744a378772c15df9c24dfa89be460ba31d8835c1
Jonas Dann <jonas.chr.dann@gmail.com>
2017-04-24 22:28:30 +0200

    Added title

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 public class IntroController : MonoBehaviour
 {
 
-    public GameObject girl;
+    public GameObject title;
 
-    private PlayerController playerController;
     private float timeLeft;
 
 	void Start ()
 	{
-	    playerController = girl.GetComponent<PlayerController>();
-	    timeLeft = 0.5f;
-	    TextController.Get().ShowText("Where is Mommy?", TextController.red, 2f);
+	    timeLeft = 3f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    playerController.forceMovement = true;
 	    timeLeft -= Time.deltaTime;
 	    if (timeLeft < 0)
 	    {
-	        playerController.forceMovement = false;
+	        title.SetActive(false);
 	        gameObject.SetActive(false);
 	    }
 	}
