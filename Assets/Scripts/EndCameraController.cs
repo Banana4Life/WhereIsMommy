25652e59109fced72a778e24606523e4e5c72978
Jonas Dann <jonas.chr.dann@gmail.com>
2017-04-24 19:08:05 +0200

    Made end scene cool

--- Assets/Scripts/EndCameraController.cs
+++ Assets/Scripts/EndCameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,16 @@ public class EndCameraController : MonoBehaviour
 {
 
     public GameObject camera;
+    public GameObject girl;
 
     private Vector3 targetPosition = new Vector3(0, 70, -80);
     private Vector3 velocityMovement = Vector3.zero;
-    private float velocityRotation;
-    private float timeLeft = 1f;
+    private float velocityRotationX;
+    private float velocityRotationZ;
+    private float timeLeft = 0.7f;
+
+    private Vector3 girlTargetPosition = new Vector3(-0.21f, 39.7f, -30.987f);
+    private float girlWalkingOutTime = 1.3f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,12 +26,21 @@ public class EndCameraController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	    timeLeft -= Time.deltaTime;
-	    if (timeLeft < 0)
+	    if (girlWalkingOutTime > 0)
 	    {
-	        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocityMovement, 2f);
-	        camera.transform.rotation = Quaternion.Euler(
-	            new Vector3(Mathf.SmoothDampAngle(camera.transform.eulerAngles.x, 25f, ref velocityRotation, 1f), 0, 0));
+	        girlWalkingOutTime -= Time.deltaTime;
+
+	        girl.transform.position = Vector3.Lerp(girl.transform.position, girlTargetPosition, Time.deltaTime / girlWalkingOutTime);
+	    }
+	    else
+	    {
+	        timeLeft -= Time.deltaTime;
+	        if (timeLeft < 0)
+	        {
+	            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocityMovement, 2f);
+	            camera.transform.rotation = Quaternion.Euler(
+	                new Vector3(Mathf.SmoothDampAngle(camera.transform.eulerAngles.x, 25f, ref velocityRotationX, 1f), 0, Mathf.SmoothDampAngle(camera.transform.eulerAngles.z, 45f, ref velocityRotationZ, 2f)));
+	        }
 	    }
 	}
 }
