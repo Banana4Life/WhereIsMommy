fc3f52870b0005da1379e191d849f3e0accd3040
Anselm Brehme <development.anselm@breh.me>
2017-04-24 19:26:39 +0200

    trigger for ending

---
+++ Assets/BigDoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDoorController : CollisionController {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override void handle(PlayerController pc)
+    {
+        if (pc.buttonsPressed == 3)
+        {
+            Debug.Log("Trigger Ending...");
+            // TODO trigger end scene
+        }
+    }
+}
