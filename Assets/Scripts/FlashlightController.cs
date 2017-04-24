6cfed1522f6208b5afdc11bb6f86793ec5fdff6b
Anselm Brehme <development.anselm@breh.me>
2017-04-24 20:33:38 +0200

    moar sounds

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -6,6 +6,8 @@ public class FlashlightController : CollisionController
 {
     public bool legendary = false;
 
+    public AudioClip pickupSound;
+
     protected override void handle(PlayerController playerController)
     {
         gameObject.SetActive(false);
@@ -18,5 +20,6 @@ public class FlashlightController : CollisionController
             playerController.Carry().carryLight = true;
         }
         TextController.Get().ShowText("Just a little more light! I hope the batteries don't run out.", TextController.red, 4f);
+        playerController.gameObject.GetComponent<AudioSource>().PlayOneShot(pickupSound);
     }
 }
--- Assets/Scripts/TeddyController.cs
+++ Assets/Scripts/TeddyController.cs
@@ -5,14 +5,15 @@ using UnityEngine.UI;
 
 public class TeddyController : CollisionController
 {
-    public string text = "Now I have my teddy. I am ready to go.";
-
     private float teddyTextTimeLeft = 0;
+
+    public AudioClip pickupSound;
 
     protected override void handle(PlayerController playerController)
     {
         gameObject.SetActive(false);
-        TextController.Get().ShowText(text, TextController.red, 4f);
+        TextController.Get().ShowText( "Now I have my teddy. I am ready to go.", TextController.red, 4f);
         playerController.Carry().carryTeddy = true;
+        playerController.gameObject.GetComponent<AudioSource>().PlayOneShot(pickupSound);
     }
 }
